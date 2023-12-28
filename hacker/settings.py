580506044e4e89264608dcd33d3312bdb5383f82
wzh <as456741@qq.com>
2023-12-28 19:37:35 +0800

    cookies 修改

--- hacker/settings.py
+++ hacker/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:8080',)
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:8080',]
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
